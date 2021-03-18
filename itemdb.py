854a61f01b538ec4b03f9f499c14a7d21193f29f
Almar Klein <almar@almarklein.org>
2021-03-18 22:26:02 +0100

    bump version

--- itemdb.py
+++ itemdb.py
@@ -18,7 +18,7 @@ import sqlite3
 import threading
 
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 version_info = tuple(map(int, __version__.split(".")))
 
 __all__ = ["ItemDB", "AsyncItemDB", "asyncify"]
