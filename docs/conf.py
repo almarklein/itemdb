524e0b6b4caded6a53ca6d3f17f675235eefdffe
Almar Klein <almar@almarklein.org>
2024-04-18 16:26:29 +0200

    Fix docbuild and copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import itemdb  # noqa: E402
 # -- Project information -----------------------------------------------------
 
 project = "itemdb"
-copyright = "2018-2023, Almar Klein"
+copyright = "2018-2024, Almar Klein"
 author = "Almar Klein"
 release = itemdb.__version__
 
@@ -58,4 +58,4 @@ master_doc = "index"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
